f25dafa31875998ea651b5de7d466d10b0e4713e
Arthur Dias <fdias.arthur@gmail.com>
2026-02-27 21:34:22 -0300

    refactor: remove unused AppController and AppService from the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ClientsModule } from './clients/clients.module';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost/nest'), ClientsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
